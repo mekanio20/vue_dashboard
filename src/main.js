c22ab4ec6a96f2e805d751ac6615ac54e7c3cae9
mekanio20 <mrxyok138@gmail.com>
2024-02-27 11:56:53 +0500

    chart test

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import router from './router/router'
 import { appAxios } from './api'
 import store from './store'
 import './style.css'
+import VueApexCharts from "vue3-apexcharts";
 
 const app = createApp(App)
     
 app.use(router)
 app.use(store)
+app.use(VueApexCharts)
 app.config.globalProperties.$appAxios = appAxios
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,51 @@
   <div>
     <Header />
     <Navbar />
+    <div class="page-header d-print-none mt-3">
+      <div class="container-xl">
+        <div class="row g-2 align-items-center">
+          <div class="col">
+            <h2 class="page-title">
+              Home
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="page-body">
+      <div class="container-xl">
+        <div class="row row-cards">
+          <div class="col-lg-6">
+            <div class="card">
+              <div class="card-body">
+                <div class="d-flex">
+                  <h3 class="card-title">Ulanyjylar</h3>
+                </div>
+                <div class="row">
+                  <div class="col">
+                    <apexchart width="580" type="line" :options="users.options" :series="users.series"></apexchart>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="card">
+              <div class="card-body">
+                <div class="d-flex">
+                  <h3 class="card-title">Girdejiler</h3>
+                </div>
+                <div class="row">
+                  <div class="col">
+                    <apexchart width="580" type="bar" :options="income.options" :series="income.series"></apexchart>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,14 +57,36 @@ export default {
   name: "Home",
   components: {
     Header,
-    Navbar,
+    Navbar
   },
   data() {
-    return {};
+    return {
+      users: {
+        options: {
+          xaxis: {
+            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+          }
+        },
+        series: [{
+          name: 'series-1',
+          data: [30, 40, 45, 50, 49, 60, 70, 91]
+        }]
+      },
+      income: {
+        options: {
+          xaxis: {
+            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+          }
+        },
+        series: [{
+          name: 'series-2',
+          data: [30, 40, 45, 50, 49, 60, 70, 91]
+        }]
+      }
+    }
   },
 };
 </script>
-
 <style>
 @import "@/assets/css/tabler.min.css";
 @import "@/assets/css/tabler-flags.min.css";
